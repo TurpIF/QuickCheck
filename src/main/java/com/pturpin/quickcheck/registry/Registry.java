8292d200cc439d24eda6faf9230f2e5b0827ffc2
Pierre Turpin <pierre.turpin54@gmail.com>
2017-04-27 14:55:17 +0200

    Declare Registry interface

---
+++ src/main/java/com/pturpin/quickcheck/registry/Registry.java
@@ -0,0 +1,11 @@
+package com.pturpin.quickcheck.registry;
+
+import com.pturpin.quickcheck.generator.Generator;
+import com.pturpin.quickcheck.identifier.TypeIdentifier;
+
+import java.util.Optional;
+
+@FunctionalInterface
+public interface Registry {
+    <T> Optional<Generator<T>> lookup(TypeIdentifier<T> identifier);
+}
