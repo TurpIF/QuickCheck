9c96932492142fdcf5eeeac1ea462f232873ba36
Pierre Turpin <pierre.turpin54@gmail.com>
2017-05-28 00:16:23 +0200

    Add more test samples

--- quickcheck-junit4/src/test/java/fr/pturpin/quickcheck/sample/QuickCheckSample_UT.java
+++ quickcheck-junit4/src/test/java/fr/pturpin/quickcheck/sample/QuickCheckSample_UT.java
@@ -1,6 +1,7 @@
 package fr.pturpin.quickcheck.sample;
 
 import fr.pturpin.quickcheck.annotation.Doubles;
+import fr.pturpin.quickcheck.annotation.Ints;
 import fr.pturpin.quickcheck.annotation.Nullable;
 import fr.pturpin.quickcheck.junit4.QuickCheck;
 import fr.pturpin.quickcheck.test.TestResult;
@@ -8,6 +9,8 @@ import fr.pturpin.quickcheck.test.configuration.TestConfiguration;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static fr.pturpin.quickcheck.test.TestResult.when;
 
 /**
  * Created by turpif on 28/04/17.
@@ -34,9 +37,26 @@ public class QuickCheckSample_UT {
   @Test
   @TestConfiguration.Skipped(0.75)
   public TestResult toStringThenParseShouldBeIdentity(@Nullable Double value) {
-    return TestResult.when(value != null, () -> {
+    return when(value != null, () -> {
       double parsed = Double.parseDouble(value.toString());
       Assert.assertEquals(value, parsed, 0.d);
     });
+  }
+
+  @Test
+  public void absoluteValueWithoutBranchingShouldBeEqualToMathAbs(int value) {
+    Assert.assertEquals(Math.abs(value), myAbs(value));
+  }
+
+  @Test
+  public TestResult absoluteValueShouldBePositive(@Ints.Extra int value) {
+    return when(value != Integer.MIN_VALUE, () -> {
+      Assert.assertTrue(myAbs(value) >= 0);
+    });
+  }
+
+  private static int myAbs(int value) {
+    int mask = value >> (Integer.SIZE - 1);
+    return (value + mask) ^ mask;
   }
 }
