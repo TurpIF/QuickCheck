78140727db0a3ddb0d76cab6fe83bde9238be674
Pierre Turpin <pierre.turpin54@gmail.com>
2017-06-04 22:47:42 +0200

    Add class id factory to shorten declaration

--- quickcheck/src/main/java/fr/pturpin/quickcheck/test/configuration/DefaultRegistryFactory.java
+++ quickcheck/src/main/java/fr/pturpin/quickcheck/test/configuration/DefaultRegistryFactory.java
@@ -5,7 +5,6 @@ import fr.pturpin.quickcheck.base.Ranges.Range;
 import fr.pturpin.quickcheck.generator.Generator;
 import fr.pturpin.quickcheck.generator.NumberGens;
 import fr.pturpin.quickcheck.generator.collection.ListGens;
-import fr.pturpin.quickcheck.identifier.ClassIdentifier;
 import fr.pturpin.quickcheck.identifier.ParametrizedIdentifier;
 import fr.pturpin.quickcheck.registry.Registries;
 import fr.pturpin.quickcheck.registry.Registry;
@@ -13,6 +12,8 @@ import fr.pturpin.quickcheck.registry.Registry;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
+
+import static fr.pturpin.quickcheck.identifier.ClassIdentifier.classId;
 
 /**
  * Created by turpif on 28/04/17.
@@ -34,16 +35,16 @@ public final class DefaultRegistryFactory implements RegistryFactory {
         BigDecimal.valueOf(Double.MIN_VALUE).multiply(BigDecimal.valueOf(1000)));
 
     return Registries.builder()
-        .put(new ClassIdentifier<>(double.class), NumberGens.doubleGen())
-        .put(new ClassIdentifier<>(Double.class), NumberGens.doubleGen())
-        .put(new ClassIdentifier<>(int.class), NumberGens.integerGen())
-        .put(new ClassIdentifier<>(Integer.class), NumberGens.integerGen())
-        .put(new ClassIdentifier<>(long.class), NumberGens.longGen())
-        .put(new ClassIdentifier<>(Long.class), NumberGens.longGen())
-        .put(new ClassIdentifier<>(BigInteger.class), NumberGens.bigIntegerGen(bigIntegerRange))
-        .put(new ClassIdentifier<>(BigDecimal.class), NumberGens.bigDecimalGen(bigDecimalRange))
+        .put(classId(double.class), NumberGens.doubleGen())
+        .put(classId(Double.class), NumberGens.doubleGen())
+        .put(classId(int.class), NumberGens.integerGen())
+        .put(classId(Integer.class), NumberGens.integerGen())
+        .put(classId(long.class), NumberGens.longGen())
+        .put(classId(Long.class), NumberGens.longGen())
+        .put(classId(BigInteger.class), NumberGens.bigIntegerGen(bigIntegerRange))
+        .put(classId(BigDecimal.class), NumberGens.bigDecimalGen(bigDecimalRange))
         .put(ParametrizedIdentifier.create((Class<List<Double>>) (Class) List.class, Double.class), (Registry registry) ->
-            registry.lookup(new ClassIdentifier<>(Double.class)).map(doubleGen -> {
+            registry.lookup(classId(Double.class)).map(doubleGen -> {
               Generator<Integer> sizeGen = NumberGens.integerGen(Ranges.closed(0, 100));
               return ListGens.arrayListGen(doubleGen, sizeGen);
             }))
